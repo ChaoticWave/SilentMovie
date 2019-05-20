630a514e0af5179c7c6bb5a4eb74dc2e99589299
ChaoticWave <chaoticwaver@gmail.com>
2019-05-20 08:48:02 -0400

    Current work checkpoint. See diff for details.

--- resources/views/layout/gs-inner.blade.php
+++ resources/views/layout/gs-inner.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $pageTitle or 'Silent Movie' }}</title>
+    <title>{{ $pageTitle ?? 'Silent Movie' }}</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="vendor/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
